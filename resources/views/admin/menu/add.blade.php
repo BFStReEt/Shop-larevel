86fa7c43faafcc03300df1652235beb0e0f2ffe3
Nguyen Anh Toi <toidxbp02@gmail.com>
2024-08-24 12:19:27 +0700

    Footer and header

--- resources/views/admin/menu/add.blade.php
+++ resources/views/admin/menu/add.blade.php
@@ -1,5 +1,63 @@
-@extends
+@extends("admin.main")
+
+@section("head")
+<script src="https://cdn.ckeditor.com/ckeditor5/35.3.0/classic/ckeditor.js"></script>
+@endsection
 
 @section("content")
-No way
+<form action="" method="POST">
+        <div class="card-body">
+
+            <div class="form-group">
+                <label for="menu">Tên Danh Mục</label>
+                <input type="text" name="name" class="form-control"  placeholder="Nhập tên danh mục">
+            </div>
+
+            <div class="form-group">
+                <label>Danh Mục</label>
+                <select class="form-control" name="parent_id">
+                    <option value="0"> Danh Mục Cha </option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label>Mô Tả </label>
+                <textarea name="description" class="form-control"></textarea>
+            </div>
+
+            <div class="form-group">
+                <label>Mô Tả Chi Tiết</label>
+                <textarea name="content" id="content" class="form-control"></textarea>
+            </div>
+
+
+            <div class="form-group">
+                <label>Kích Hoạt</label>
+                <div class="custom-control custom-radio">
+                    <input class="custom-control-input" value="1" type="radio" id="active" name="active" checked="">
+                    <label for="active" class="custom-control-label">Có</label>
+                </div>
+                <div class="custom-control custom-radio">
+                    <input class="custom-control-input" value="0" type="radio" id="no_active" name="active" >
+                    <label for="no_active" class="custom-control-label">Không</label>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="card-footer">
+            <button type="submit" class="btn btn-primary">Tạo Danh Mục</button>
+        </div>
+        @csrf
+    </form>
+@endsection
+
+@section("footer")
+<script>
+    ClassicEditor
+        .create(document.querySelector('#content'))
+        .catch(error => {
+            console.error(error);
+        });
+</script>
 @endsection
