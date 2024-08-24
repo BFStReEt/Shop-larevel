bdfc68f6784b200643207b2a62a90aeea2071eaf
Nguyen Anh Toi <toidxbp02@gmail.com>
2024-08-24 10:48:31 +0700

    Chỉnh phần sidebard.blade.php

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -47,7 +47,7 @@
                                 <h3 class="card-title">{{ $title }}</h3>
                             </div>
 
-                            
+
                             @yield('content')
 
                         </div>
@@ -70,9 +70,9 @@
     <!-- /.content-wrapper -->
     <footer class="main-footer">
         <div class="float-right d-none d-sm-block">
-            <b>Version</b> 3.1.0
+            <b>Version</b> 1.0
         </div>
-        <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+        <strong>Copyright &copy; 2024 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
     </footer>
 </div>
 <!-- ./wrapper -->
