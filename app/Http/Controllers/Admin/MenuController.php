aa807d48234ba240189ff888aa0f196611e866b8
Nguyen Anh Toi <toidxbp02@gmail.com>
2024-08-26 11:06:28 +0700

    Hello

--- app/Http/Controllers/Admin/MenuController.php
+++ app/Http/Controllers/Admin/MenuController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Menu\CreateFormRequest;
 use Illuminate\Http\Request;
-
+//hello
 class MenuController extends Controller
 {
     protected $menuService;
